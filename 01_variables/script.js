abbdaeb0d71b894836cb62ad1626c5bccd96f49c
LakshmiSagar JM <lakshmisagar.jm@mphasis.com>
2025-04-10 09:48:00 +0530

    completed 01 and 02

---
+++ 01_variables/script.js
@@ -0,0 +1,5 @@
+let username = "Lakshmisagar J M";
+let age = 23;
+let isStudent = true;
+
+console.log(`hii, i'm ${username}, my age is ${age} and i'm ${age < 18 && isStudent? 'a student ' : 'not a student'}`);
