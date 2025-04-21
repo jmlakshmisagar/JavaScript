9fc9cc513954759bd5f50400760d503693542998
lxmpco <lakshmisagar.jm@mphasis.com>
2025-04-21 23:42:38 +0530

    conditions

---
+++ 29_if-else/script.js
@@ -0,0 +1,5 @@
+const username = prompt("Enter your username") || "lx7";
+const password = prompt("Enter your password") || "1234";
+
+console.log(`name id ${username}`);
+console.log(`password is ${password}`);
