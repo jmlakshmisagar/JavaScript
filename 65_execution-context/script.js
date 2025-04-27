d5bc5a7548ad68f5235af4034bceb99a6a137f25
lxmpco <lakshmisagar.jm@mphasis.com>
2025-04-27 23:37:07 +0530

    to-do

---
+++ 65_execution-context/script.js
@@ -0,0 +1,19 @@
+debugger
+
+console.log("started");
+
+sayHii()
+
+var a = 10;
+var b = 20
+
+function sayHii() {
+    console.log('Hii');
+    console.log(add(1, 2));
+    console.log('Ended');
+}
+
+function add(x, y) {
+    return x + y
+}
+
